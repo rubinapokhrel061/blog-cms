81f37b2dd60ebebd72c15056d71bb9eb8faa6e92
Rubina Pokhrel <pokhrelrubina2061@gmail.com>
2025-06-14 06:52:49 +0545

    readme update

--- Server/routes/postRoutes.js
+++ Server/routes/postRoutes.js
@@ -3,11 +3,9 @@ import PostController from "../controllers/postController.js";
 
 const router = express.Router();
 
-// Public routes
 router.get("/", PostController.getAllPosts);
 router.get("/:id", PostController.getPostById);
 
-// Protected/admin routes
 router.post("/", PostController.createPost);
 router.patch("/:id", PostController.updatePost);
 router.delete("/:id", PostController.deletePost);
